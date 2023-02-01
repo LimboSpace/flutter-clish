835817e5767b3d7b889a8259f14e7aeaf2787457
LiamMarega <liammarega85@gmail.com>
2023-02-01 18:16:36 -0300

    Connect endpoints btcPrice, dolarModal

--- lib/bloc/cubit/offers_cubit.dart
+++ lib/bloc/cubit/offers_cubit.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:ffi';
 
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
@@ -40,10 +41,23 @@ class OffersCubit extends Cubit<OffersState> {
       Response response = await dio
           .get('https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDC');
 
-      List offerList = response.data;
+      double btcPrice = response.data['price'];
 
-      emit(state.copyWith(allOffers: offerList));
-      return response.data['data']['ad_list'];
+      emit(state.copyWith(btcPrice: btcPrice));
+    } catch (e) {
+      if (e is DioError) {}
+    }
+  }
+
+  getDolarModal(context) async {
+    try {
+      Dio dio = getDio();
+
+      Response response = await dio
+          .get('https://mercados.ambito.com//dolar/informal/variacion');
+
+      String dolar = response.data['compra'];
+      emit(state.copyWith(dolarModal: dolar));
     } catch (e) {
       if (e is DioError) {}
     }
--- lib/bloc/cubit/offers_state.dart
+++ lib/bloc/cubit/offers_state.dart
@@ -5,7 +5,7 @@ class OffersState extends Equatable {
     this.allOffers, {
     this.showInvalids = true,
     this.btcPrice = 0,
-    this.dolarModal = 0,
+    this.dolarModal = '',
     this.minVolume = 0,
     this.lastUpdate = 0,
   });
@@ -14,15 +14,25 @@ class OffersState extends Equatable {
   List? allOffers;
   bool showInvalids;
   double btcPrice;
-  double dolarModal;
+  String dolarModal;
   double minVolume;
   int lastUpdate;
 
   OffersState copyWith({
     List? allOffers,
+    bool? showInvalids,
+    double? btcPrice,
+    double? minVolume,
+    int? lastUpdate,
+    String? dolarModal,
   }) {
     return OffersState(
       allOffers ?? this.allOffers,
+      showInvalids: showInvalids ?? this.showInvalids,
+      btcPrice: btcPrice ?? this.btcPrice,
+      dolarModal: dolarModal ?? this.dolarModal,
+      minVolume: minVolume ?? this.minVolume,
+      lastUpdate: lastUpdate ?? this.lastUpdate,
     );
   }
 
